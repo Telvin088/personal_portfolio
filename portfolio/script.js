b5aaec95df258956aa2fd2427badb12bb46cd148
Telvin <telvinngichukii@gmail.com>
2024-05-10 09:47:43 +0300

    new changes

--- portfolio/script.js
+++ portfolio/script.js
@@ -4,4 +4,12 @@ git status
 git commit -m ""
 git status
 
+
+// add new branch
+git checkout -b {name_of_branch}
+// switch between branches
+git checkout {name_of_branch}
 new branch
+
+// merge branches
+git merge master/{name_of_branch}
